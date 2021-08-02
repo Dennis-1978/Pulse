31e203af272cdedc5ef576bcd467edd5ab7ab6e3
Denis <d.khokhloff2011@yandex.ru>
2021-08-02 21:20:10 +0300

    added a section with a slider, adapted

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+    responsive: {
+        320: {
+            nav: true
+        },
+        768: {
+            nav: false
+        }
+    }
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
